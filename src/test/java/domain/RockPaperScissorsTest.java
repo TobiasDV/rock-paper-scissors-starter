e7c044b9351e1dc51324b76d1e4b244dea912d3c
Tobias de Vries <tobias.de.vries@live.nl>
2020-12-08 15:36:05 +0100

    Testen met tobias deel 1

--- src/test/java/domain/RockPaperScissorsTest.java
+++ src/test/java/domain/RockPaperScissorsTest.java
@@ -1,14 +1,34 @@
 package domain;
 
+import domain.rules.RockPaperScissors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class RockPaperScissorsTest {
+class RockPaperScissorsTest {
+
     @Test
-    @DisplayName("Remove this test when setup works")
-    public void temporaryTest() {
-        assertTrue(false, "If you see this, it works and you can remove this method!");
+    @DisplayName("Rock exterminates scissors")
+    void rockExterminatesScissors(){
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.ROCK, Move.SCISSORS);
+        assertEquals(Result.WIN, result);
+    }
+
+    @Test
+    @DisplayName("Scissor obliterates paper")
+    void scissorsObliteratesPaper(){
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.SCISSORS, Move.PAPER);
+        assertEquals(Result.WIN, result);
+    }
+
+    @Test
+    @DisplayName("Paper purges rock")
+    void paperPurgesRock(){
+        Rules game = new RockPaperScissors();
+        Result result = game.decide(Move.PAPER, Move.ROCK);
+        assertEquals(Result.WIN, result);
     }
 }
