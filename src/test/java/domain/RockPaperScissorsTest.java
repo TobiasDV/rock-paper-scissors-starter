ab28a85fe45f6ec07dc0cdf60e1cf83f49e5650d
Tobias de Vries <tobias.de.vries@live.nl>
2020-12-08 16:02:23 +0100

    Improved rockpaperscissors test

--- src/test/java/domain/RockPaperScissorsTest.java
+++ src/test/java/domain/RockPaperScissorsTest.java
@@ -3,32 +3,41 @@ package domain;
 import domain.rules.RockPaperScissors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class RockPaperScissorsTest {
 
-    @Test
-    @DisplayName("Rock exterminates scissors")
-    void rockExterminatesScissors(){
-        Rules game = new RockPaperScissors();
-        Result result = game.decide(Move.ROCK, Move.SCISSORS);
-        assertEquals(Result.WIN, result);
+    private static Stream<Arguments> provideMovesAndResults(){
+        return Stream.of(
+                // Winning moves
+                Arguments.of(Move.ROCK, Move.SCISSORS, Result.WIN),
+                Arguments.of(Move.PAPER, Move.ROCK, Result.WIN),
+                Arguments.of(Move.SCISSORS, Move.PAPER, Result.WIN),
+                // Draw moves
+                Arguments.of(Move.ROCK, Move.ROCK, Result.DRAW),
+                Arguments.of(Move.PAPER, Move.PAPER, Result.DRAW),
+                Arguments.of(Move.SCISSORS, Move.SCISSORS, Result.DRAW),
+                // Losing moves
+                Arguments.of(Move.SCISSORS, Move.ROCK, Result.LOSE),
+                Arguments.of(Move.ROCK, Move.PAPER, Result.LOSE),
+                Arguments.of(Move.PAPER, Move.SCISSORS, Result.LOSE)
+        );
     }
 
-    @Test
-    @DisplayName("Scissor obliterates paper")
-    void scissorsObliteratesPaper(){
+    @ParameterizedTest
+    @MethodSource("provideMovesAndResults")
+    void verifyMovesAndResults(Move mine, Move theirs, Result
+            expectedResult) {
         Rules game = new RockPaperScissors();
-        Result result = game.decide(Move.SCISSORS, Move.PAPER);
-        assertEquals(Result.WIN, result);
+        Result result = game.decide(mine, theirs);
+        assertEquals(expectedResult, result);
     }
 
-    @Test
-    @DisplayName("Paper purges rock")
-    void paperPurgesRock(){
-        Rules game = new RockPaperScissors();
-        Result result = game.decide(Move.PAPER, Move.ROCK);
-        assertEquals(Result.WIN, result);
-    }
+
 }
